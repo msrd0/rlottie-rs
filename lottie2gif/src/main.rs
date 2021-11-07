c954355ddc9cc79242ae4beb879bbbd3067851f3
Dominic <git@msrd0.de>
2021-11-07 13:40:03 +0100

    first take at lottie2gif

---
+++ lottie2gif/src/main.rs
@@ -0,0 +1,99 @@
+use clap::Parser;
+use gif::Frame;
+use rlottie::{Animation, Size};
+use std::{fs::File, path::Path};
+
+#[derive(Parser)]
+struct Args {
+	#[clap(name = "lottieFileName")]
+	lottie_file_name: String,
+
+	#[clap(name = "resolution", default_value = "200x200")]
+	resolution: String,
+
+	#[clap(name = "bgColor", default_value = "ffffffff")]
+	bg_color: String
+}
+
+fn main() {
+	let args = Args::parse();
+
+	let path = args.lottie_file_name;
+	let path: &Path = path.as_ref();
+	let (width, height): (u16, u16) = {
+		let mut split = args.resolution.split('x');
+		(
+			split.next().unwrap().parse().expect("Invalid resolution"),
+			split
+				.next()
+				.expect("Resolution must be of form <width>x<height>")
+				.parse()
+				.expect("Invalid resolution")
+		)
+	};
+	let bg_color = u32::from_str_radix(&args.bg_color, 16).expect("Invalid bgColor");
+	let bg_r = ((bg_color >> 16) & 0xFF) as u8;
+	let bg_g = ((bg_color >> 8) & 0xFF) as u8;
+	let bg_b = (bg_color & 0xFF) as u8;
+
+	let mut player = Animation::from_file(path).expect("Failed to open file");
+	let buffer_len = width as usize * height as usize;
+	let mut buffer32 = Vec::with_capacity(buffer_len);
+	let mut buffer8 = unsafe {
+		buffer32.set_len(buffer_len);
+		Vec::from_raw_parts(
+			buffer32.as_mut_ptr() as *mut u8,
+			buffer_len * 4,
+			buffer_len * 4
+		)
+	};
+	let frame_count = player.totalframe();
+
+	let mut gif = gif::Encoder::new(
+		File::create(path.join(".gif")).expect("Failed to create output file"),
+		width,
+		height,
+		&[]
+	)
+	.expect("Failed to create gif");
+	for frame in 0..frame_count {
+		player.render(
+			frame,
+			&mut buffer32,
+			Size {
+				width: width as _,
+				height: height as _
+			},
+			width as u64 * 4
+		);
+
+		for i in 0..buffer_len {
+			let a = buffer8[i * 4 + 3];
+			if a != 0 {
+				let r = buffer8[i * 4 + 2];
+				let g = buffer8[i * 4 + 1];
+				let b = buffer8[i * 4];
+
+				if a != 0xFF {
+					// un premultiply
+					let r2 = (bg_r as f32 * (0xFF - a) as f32 / 255.0) as u8;
+					let g2 = (bg_g as f32 * (0xFF - a) as f32 / 255.0) as u8;
+					let b2 = (bg_b as f32 * (0xFF - a) as f32 / 255.0) as u8;
+					buffer8[i * 4] = r + r2;
+					buffer8[i * 4 + 1] = g + g2;
+					buffer8[i * 4 + 2] = b + b2;
+				} else {
+					buffer8[i * 4] = r;
+					buffer8[i * 4 + 2] = b;
+				}
+			} else {
+				buffer8[i * 4 + 2] = bg_b;
+				buffer8[i * 4 + 1] = bg_g;
+				buffer8[i * 4] = bg_r;
+			}
+		}
+
+		gif.write_frame(&Frame::from_rgba(width, height, &mut buffer8))
+			.expect("Failed to write frame");
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use rlottie_sys::*;
-use std::{ffi::CString, os::unix::ffi::OsStrExt, path::Path};
+use std::{ffi::CString, os::unix::ffi::OsStrExt, path::Path, ptr};
 
 fn path_to_cstr<P>(path: P) -> CString
 where
@@ -11,13 +11,13 @@ where
 
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Size {
-	width: u64,
-	height: u64
+	pub width: u64,
+	pub height: u64
 }
 
-pub struct LottieAnimation(*mut Lottie_Animation_S);
+pub struct Animation(*mut Lottie_Animation_S);
 
-impl Drop for LottieAnimation {
+impl Drop for Animation {
 	fn drop(&mut self) {
 		unsafe {
 			lottie_animation_destroy(self.0);
@@ -25,25 +25,27 @@ impl Drop for LottieAnimation {
 	}
 }
 
-impl LottieAnimation {
-	pub fn from_file<P>(path: P) -> Self
+impl Animation {
+	pub fn from_file<P>(path: P) -> Option<Self>
 	where
 		P: AsRef<Path>
 	{
 		let path = path_to_cstr(path);
-		Self(unsafe { lottie_animation_from_file(path.as_ptr()) })
+		let ptr = unsafe { lottie_animation_from_file(path.as_ptr()) };
+		(ptr != ptr::null_mut()).then(|| Self(ptr))
 	}
 
-	pub fn from_data<P>(data: String, key: String, resource_path: P) -> Self
+	pub fn from_data<P>(data: String, key: String, resource_path: P) -> Option<Self>
 	where
 		P: AsRef<Path>
 	{
 		let data = CString::new(data).expect("data must not contain nul");
 		let key = CString::new(key).expect("key must not contain nul");
 		let resource_path = path_to_cstr(resource_path);
-		Self(unsafe {
+		let ptr = unsafe {
 			lottie_animation_from_data(data.as_ptr(), key.as_ptr(), resource_path.as_ptr())
-		})
+		};
+		(ptr != ptr::null_mut()).then(|| Self(ptr))
 	}
 
 	pub fn size(&self) -> Size {
@@ -100,31 +102,6 @@ impl LottieAnimation {
 				size.height,
 				bytes_per_line
 			);
-		}
-	}
-
-	pub fn render_async(
-		&mut self,
-		frame_num: u64,
-		buffer: &mut Vec<u32>,
-		size: Size,
-		bytes_per_line: u64
-	) {
-		unsafe {
-			lottie_animation_render_async(
-				self.0,
-				frame_num,
-				buffer.as_mut_ptr(),
-				size.width,
-				size.height,
-				bytes_per_line
-			);
-		}
-	}
-
-	pub fn render_flush(&mut self) {
-		unsafe {
-			lottie_animation_render_flush(self.0);
 		}
 	}
 }
