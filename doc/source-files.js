888e9b0fa8c02fc723441182a453f2f28c858763
msrd0 <msrd0@users.noreply.github.com>
2022-03-28 09:21:48 +0000

    GitHub Pages for 2a20352361c57611f45dd712657b9ea7285bba46

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,5 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["lottie2gif"] = {"name":"","files":["lib.rs","util.rs"]};
+sourcesIndex["lottie2webp"] = {"name":"","files":["lib.rs","util.rs"]};
 sourcesIndex["rlottie"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["rlottie_sys"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
