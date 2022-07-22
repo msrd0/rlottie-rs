203744ddf2893f2ea4dc5a5c05d1d7b52cba8e19
msrd0 <msrd0@users.noreply.github.com>
2022-07-22 12:59:30 +0000

    GitHub Pages for ca1c47734a1d17a28453d2a77c6751bdd6e16d78

--- doc/implementors/core/cmp/trait.Ord.js
+++ doc/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rlottie"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"rlottie/struct.Size.html\" title=\"struct rlottie::Size\">Size</a>","synthetic":false,"types":["rlottie::Size"]}];
+implementors["rlottie"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"rlottie/struct.Size.html\" title=\"struct rlottie::Size\">Size</a>","synthetic":false,"types":["rlottie::Size"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
