7b55a0a18127455a4910ebfe5b6cece4a648007b
msrd0 <msrd0@users.noreply.github.com>
2023-02-07 13:30:06 +0000

    GitHub Pages for e141b0a58267b9b5e04b2e2e2dc3030a08d9a738

--- doc/implementors/core/cmp/trait.PartialOrd.js
+++ doc/implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rlottie":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"rlottie/struct.Size.html\" title=\"struct rlottie::Size\">Size</a>&gt; for <a class=\"struct\" href=\"rlottie/struct.Size.html\" title=\"struct rlottie::Size\">Size</a>"]]
+"rlottie":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"rlottie/struct.Size.html\" title=\"struct rlottie::Size\">Size</a>&gt; for <a class=\"struct\" href=\"rlottie/struct.Size.html\" title=\"struct rlottie::Size\">Size</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/core/convert/trait.AsRef.js
+++ doc/implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rlottie":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"rlottie/struct.Surface.html\" title=\"struct rlottie::Surface\">Surface</a>"]]
+"rlottie":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"rlottie/struct.Surface.html\" title=\"struct rlottie::Surface\">Surface</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
