5133578b78aafeb26d3f046fec53ad6fbb160970
msrd0 <msrd0@users.noreply.github.com>
2023-04-17 16:43:10 +0000

    GitHub Pages for 25f4855603df34bbfd3c992f853b5805b8a8d918

--- doc/implementors/core/convert/trait.AsRef.js
+++ doc/implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rlottie":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.1/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"rlottie/struct.Surface.html\" title=\"struct rlottie::Surface\">Surface</a>"]]
+"rlottie":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.68.2/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"rlottie/struct.Surface.html\" title=\"struct rlottie::Surface\">Surface</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
